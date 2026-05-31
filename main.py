be08304b01508e68e48f3edc7a527a159645a37b
Sampath2703 <naniganji2703@gmail.com>
2026-05-31 10:22:24 +0530

    changed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI,Request 
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 import ast
 from openai import OpenAI
+import os
 
 app = FastAPI()
 
@@ -17,7 +19,7 @@ app.add_middleware(
 
 
 client = OpenAI(
-    api_key = "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",
+    api_key = os.getenv("GROQ_API_KEY"),
     base_url="https://api.groq.com/openai/v1"
 )
 @app.post("/generate")
